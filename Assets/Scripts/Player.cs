1b28f80e28015eedb2204c2201671eca689a1fa1
Alpaca <mihaililyin08@gmail.com>
2024-03-28 20:05:44 +0400

    start2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpforce = 5f;
     public Rigidbody2D rb;
+
+    public bool IsGrounded = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            IsGrounded = true;
+        }
     }
     void Update()
     {
         float MovementX = Input.GetAxis("Horizontal");
         float MovementY = Input.GetAxis("Vertical");
-
+        if(MovementX < 0 && transform.rotation != Quaternion.Euler(0, 180, 0))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (MovementX > 0 && transform.rotation != Quaternion.Euler(0, 0, 0))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
         rb.velocity = new Vector2(MovementX, MovementY).normalized * speed;
+
+        //if (Input.GetKey(KeyCode.Space) && IsGrounded)
+        //{
+        //    IsGrounded = false;
+        //    rb.AddForce(Vector2.up * jumpforce);
+        //}
     }
 }
