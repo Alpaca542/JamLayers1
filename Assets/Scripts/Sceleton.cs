700f4e597c67264f4ab566be56ca4ff5f06d9465
Alpaca <mihaililyin08@gmail.com>
2024-03-30 10:46:19 +0400

    NewAI

--- Assets/Scripts/Sceleton.cs
+++ Assets/Scripts/Sceleton.cs
@@ -6,40 +6,75 @@ using NavMeshPlus;
 
 public class Sceleton : MonoBehaviour
 {
+    [Header("System")]
     public NavMeshAgent agent;
-
     public Rigidbody2D rb;
-
     public Animator anim;
-
     public GameObject rayFinder;
-
     public GameObject player;
+
+    [Header("Debug")]
+    public bool AmIChasing;
+
+    [Header("Walknig")]
+    public GameObject start;
+    public GameObject finish;
+    public bool ToFinish = true;
 
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
 
     public void SawAPlayer(GameObject Target)
     {
-        agent.SetDestination(Target.transform.position);
+        AmIChasing = true;
+        CancelInvoke(nameof(StopChasing));
+        Invoke(nameof(StopChasing), 3f);
     }
-
+    void StopChasing()
+    {
+        AmIChasing = false;
+    }
     private void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If we touch finsh && ToFinish
+        if((collision.gameObject == finish && ToFinish) || (collision.gameObject == start && !ToFinish))
+        {
+            ToFinish = !ToFinish;
+        }
+    }
     private void Update()
     {
+        //Chase if the player is nearby
+        if (AmIChasing)
+        {
+            agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+        }
+        else
+        {
+            if (ToFinish)
+            {
+                agent.SetDestination(finish.transform.position);
+            }
+            else
+            {
+                agent.SetDestination(start.transform.position);
+            }
+
+        }
+
+
         //Rotate to the player's side
         if (agent.velocity.x > 0.2 && transform.rotation != Quaternion.Euler(new Vector3(0, 0, 0)))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            //rayFinder.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
         }
         else if(agent.velocity.x < -0.2 && transform.rotation != Quaternion.Euler(new Vector3(0, 0, -180)) && transform.rotation != Quaternion.Euler(new Vector3(0, 0, 180)))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-            //rayFinder.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
         }
     }
 }
