b9838c86cf6111c482c0757c0d405bcd39b74e7a
Alpaca <mihaililyin08@gmail.com>
2024-04-01 12:09:29 +0400

    Fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -14,7 +16,7 @@ public class Player : MonoBehaviour
     public Animator anim1;
     public AnimationClip clipDie;
     public DieManager diemng;
-
+    public GameObject CurrentDoor;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -32,6 +34,32 @@ public class Player : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         rb.velocity = new Vector2(MovementX, MovementY).normalized * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(DeleteDoor());
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopCoroutine(DeleteDoor());
+            CurrentDoor.GetComponent<Door>().txt.SetActive(false);
+        }
+    }
+    IEnumerator DeleteDoor()
+    {
+        CurrentDoor.GetComponent<Door>().txt.SetActive(true);
+        CurrentDoor.GetComponent<Door>().txt.GetComponent<TMP_Text>().text = "5";
+        yield return new WaitForSeconds(1f);
+        CurrentDoor.GetComponent<Door>().txt.GetComponent<TMP_Text>().text = "4";
+        yield return new WaitForSeconds(1f);
+        CurrentDoor.GetComponent<Door>().txt.GetComponent<TMP_Text>().text = "3";
+        yield return new WaitForSeconds(1f);
+        CurrentDoor.GetComponent<Door>().txt.GetComponent<TMP_Text>().text = "2";
+        yield return new WaitForSeconds(1f);
+        CurrentDoor.GetComponent<Door>().txt.GetComponent<TMP_Text>().text = "1";
+        CurrentDoor.GetComponent<BoxCollider2D>().enabled = false;
+        CurrentDoor.GetComponent<SpriteRenderer>().enabled = false;
+        Destroy(CurrentDoor);
     }
     public void die()
     {
@@ -39,6 +67,13 @@ public class Player : MonoBehaviour
         {
             AmIDead = true;
             diemng.ToTheMenu();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Door")
+        {
+            CurrentDoor = collision.gameObject;
         }
     }
 }
