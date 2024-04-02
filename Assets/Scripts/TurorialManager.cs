101dbf371a5394a7367feac8c4fac0f7e17652df
Alpaca <mihaililyin08@gmail.com>
2024-04-02 13:08:18 +0400

    DialogueStart

--- Assets/Scripts/TurorialManager.cs
+++ Assets/Scripts/TurorialManager.cs
@@ -10,19 +10,28 @@ public class TurorialManager : MonoBehaviour
     public string[] sentences;
     public Sprite[] faces;
     private int index;
+    public GameObject btnContinue;
     public float typingspeed = 0.02f;
     
     IEnumerator Type()
     {
+        btnContinue.SetActive(false);
+        Display.text = "";
         Display2.sprite = faces[index];
         foreach (char letter1 in sentences[index].ToCharArray())
         {
             Display.text += letter1;
             yield return new WaitForSeconds(typingspeed);
         }
+        btnContinue.SetActive(true);
     }
     private void Start()
     {
+        StartCoroutine(Type());
+    }
+    public void ContinueTyping()
+    {
+        index++;
         StartCoroutine(Type());
     }
 }
