098f4a1b815c762f45ad5860342d98967619a70b
Alpaca <mihaililyin08@gmail.com>
2024-04-03 19:23:21 +0400

    Fiiiix

--- Assets/Scripts/MagicManager.cs
+++ Assets/Scripts/MagicManager.cs
@@ -43,7 +43,7 @@ public class MagicManager : MonoBehaviour
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         IsInMagicMode = false;
         GlobalSun.color = new Color32(255, 255, 255, 255);
-        GlobalSun.intensity = 0.35f;
+        GlobalSun.intensity = 0.5f;
         Time.timeScale = 1;
     }
     private void Update()
@@ -54,7 +54,15 @@ public class MagicManager : MonoBehaviour
             mousePosition.z = Camera.main.nearClipPlane;
             Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             worldMousePosition.z = 0;
-            if(Physics2D.OverlapCircle(worldMousePosition, 0.01f))
+            bool allowedd = true;
+            foreach(Collider2D cl in Physics2D.OverlapCircleAll(worldMousePosition, 0.01f))
+            {
+                if(cl.gameObject.tag == "Decorations")
+                {
+                    allowedd = false;
+                }
+            }
+            if(Physics2D.OverlapCircle(worldMousePosition, 0.01f) && allowedd)
             {
                 Ducks--;
                 duckstxt.text = Ducks.ToString();
